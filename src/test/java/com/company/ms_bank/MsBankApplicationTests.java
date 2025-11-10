c827ff0d278a384fa532f4d3144db3d423d18e2e
jehison.galarza <jehisonsc045@gmail.com>
2025-11-10 12:30:54 -0500

    firts configuration

---
+++ src/test/java/com/company/ms_bank/MsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ms_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
