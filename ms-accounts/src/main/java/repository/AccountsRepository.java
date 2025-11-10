7a24e148e8046c40dee4f4d3838afaae5f338770
jehison.galarza <jehisonsc045@gmail.com>
2025-11-10 16:36:51 -0500

    domain and repositories created for both ms

---
+++ ms-accounts/src/main/java/repository/AccountsRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IAccountsRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByAccountNumber(String accountNumber);
+    
+}
